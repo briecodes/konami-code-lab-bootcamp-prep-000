26a504520887bdc76c40b30e48dd46bd87b8c91f
Brie Morales <bombibills@gmail.com>
2018-03-06 01:03:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
   let i = 0;
   window.addEventListener('keydown', function(e) {
-  console.log(e.which)
+  console.log(e.detail);
 });
 //   window.addEventListener('keydown', function(e){
 //   const key = parseInt(e.detail);
