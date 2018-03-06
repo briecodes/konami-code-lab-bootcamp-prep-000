bf66ff2431ffebea7545a59f72548fccd52a2ff6
Brie Morales <bombibills@gmail.com>
2018-03-06 01:14:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
   let i = 0;
   document.body.addEventListener('keydown', function(e){
-  const key = parseInt(e.detail || e.which);
+  const key = parseInt(e.location);
   console.log(key);
   if (key === code[i]){
     i++;
